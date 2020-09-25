b05ba4d50e3b8bbdb36cf23b34203f93651c5e9b
Nitesh Raut <rautesh6@gmail.com>
2020-09-25 19:05:53 +0545

    test commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello , This is nitesh raut.</div>;
+  return (
+    <div className="App" style={{ background: "red" }}>
+      Hello , This is nitesh raut.
+    </div>
+  );
 }
 
 export default App;
