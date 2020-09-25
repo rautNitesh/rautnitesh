3495f86a9875ad8fea0bb055e4d9390ec80ec7c4
Nitesh Raut <rautesh6@gmail.com>
2020-09-25 20:30:39 +0545

    [Added] portfolio landing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+
+import "./App.scss";
+import Hero from "./images/myimage.png";
+import Logo from "./images/my logo.jpg";
 
 function App() {
   return (
-    <div className="App" style={{ background: "red" }}>
-      Hello , This is nitesh raut.
+    <div className="App">
+      <div className="hero">
+        <div className="left-half">
+          <div className="circle">
+            <img src={Logo} className="logo" alt="my-hero" />
+          </div>
+          <img className="hero-image" src={Hero} alt="my-hero" />
+        </div>
+        <div className="right-half">
+          <h1>Hello, Everyone</h1>
+          <h3>I am Nitesh Raut.</h3>
+          <h5>Web developer, CS student</h5>
+          <p>
+            {" "}
+            I love coding especially with javascript and python , proficient
+            with both frontend and backend development with Django, Django REST,
+            Express Node, React js in the frontend but not limited and willing
+            to learn new technologies as the time demands.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
