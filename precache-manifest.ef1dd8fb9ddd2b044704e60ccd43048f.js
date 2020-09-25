03693c023e1ee11bf4e0c17269adb9f87b44e9e5
Nitesh Raut <rautesh6@gmail.com>
2020-09-25 18:54:01 +0545

    Updates

---
+++ precache-manifest.ef1dd8fb9ddd2b044704e60ccd43048f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d94d27e0127b66d35728524eb97023a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc761d662fb65f3c95a6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b0a0599983feaf2b5741",
+    "url": "/static/js/2.6c74b088.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c74b088.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc761d662fb65f3c95a6",
+    "url": "/static/js/main.00415ed2.chunk.js"
+  },
+  {
+    "revision": "1f3a0b573979da334c95",
+    "url": "/static/js/runtime-main.7d0f646a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
